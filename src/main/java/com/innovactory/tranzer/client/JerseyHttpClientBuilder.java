8201a45bb02a75e6c8c5d7235bb861f7cdfd4d64
Tristan van Vaalen <tristan@innovactory.com>
2022-10-13 15:34:25 +0200

    Stable jersey-client version
    (cherry picked from commit aa6054646ffecb427019a1d02fff27e6a2cf1e03)

--- src/main/java/com/innovactory/tranzer/client/JerseyHttpClientBuilder.java
+++ src/main/java/com/innovactory/tranzer/client/JerseyHttpClientBuilder.java
@@ -1,6 +1,7 @@
 package com.innovactory.tranzer.client;
 
 import com.innovactory.jersey.JerseyClientBuilder;
+import com.innovactory.jersey.logging.SLF4JLogAdapter;
 import org.glassfish.jersey.logging.LoggingFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +16,7 @@ public class JerseyHttpClientBuilder {
                 .poolSize(connectionPoolSize)
                 .basicAuthentication(username, password)
                 .enableJackson("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
-                .register(new LoggingFeature(new LogBridge(logger, api.getSimpleName() + baseUrl)))
+                .register(new LoggingFeature(new SLF4JLogAdapter(api.getSimpleName() + baseUrl, logger)))
                 .register(new TranzerErrorResponseFilter());
         if (clientRequestFilter != null) {
             clientBuilder = clientBuilder.register(clientRequestFilter);
