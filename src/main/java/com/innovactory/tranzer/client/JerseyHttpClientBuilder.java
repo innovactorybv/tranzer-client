3f626f7c3a1c5ca6ce8d44018005cb8f5eac97c9
Tristan van Vaalen <tristan@innovactory.com>
2023-05-11 12:51:13 +0200

    Revert "Stable jersey-client version"
    This reverts commit aa6054646ffecb427019a1d02fff27e6a2cf1e03.

--- src/main/java/com/innovactory/tranzer/client/JerseyHttpClientBuilder.java
+++ src/main/java/com/innovactory/tranzer/client/JerseyHttpClientBuilder.java
@@ -1,7 +1,6 @@
 package com.innovactory.tranzer.client;
 
 import com.innovactory.jersey.JerseyClientBuilder;
-import com.innovactory.jersey.logging.SLF4JLogAdapter;
 import org.glassfish.jersey.logging.LoggingFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +15,7 @@ public class JerseyHttpClientBuilder {
                 .poolSize(connectionPoolSize)
                 .basicAuthentication(username, password)
                 .enableJackson("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
-                .register(new LoggingFeature(new SLF4JLogAdapter(api.getSimpleName() + baseUrl, logger)))
+                .register(new LoggingFeature(new LogBridge(logger, api.getSimpleName() + baseUrl)))
                 .register(new TranzerErrorResponseFilter());
         if (clientRequestFilter != null) {
             clientBuilder = clientBuilder.register(clientRequestFilter);
